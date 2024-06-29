ed7cb8334218b7535c42042d76e6b76c6167e4d6
saadmahmood <saadmhmoood@gmail.com>
2024-06-29 13:56:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/madprojectml/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.madprojectml;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+    }
+}
